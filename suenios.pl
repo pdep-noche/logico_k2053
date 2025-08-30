877cf2b2ecaa15e251bed9f352ca4e6a3e321cf8
Valeria Pennella <valeria.pennella@newfold.com>
2025-08-29 21:59:53 -0300

    ejercicios tipo parcial

--- suenios.pl
+++ suenios.pl
@@ -36,4 +36,41 @@ nivelDificultad(futbolista(Equipo), 16):- not(equipoChico(Equipo)).
 equipoChico(arsenal).
 equipoChico(aldosivi).
 
+%%Punto 3
+tieneQuimica(Personaje, Persona):- creeEn(Persona, Personaje), 
+    cumpleCondiciones(Persona, Personaje).
+
+cumpleCondiciones(Persona, campanita):- dificultad(Persona, Dificultad), 
+    Dificultad < 5.
+cumpleCondiciones(Persona, Personaje):- Personaje \= campanita, 
+    todosLosSueniosPuros(Persona), not(esAmbicioso(Persona)).
+
+todosLosSueniosPuros(Persona):- forall(quiere(Persona, Suenio), esPuro(Suenio)).
+
+esPuro(futbolista(_)).
+esPuro(cantante(Discos)):- Discos < 200000.
+
+%Punto 4
+puedeAlegrar(Personaje, Persona):-quiere(Persona,_),
+    tieneQuimica(Personaje, Persona), 
+    cumpleCondicionesParaAlegrar(Personaje).
+
+cumpleCondicionesParaAlegrar(Personaje):-sano(Personaje).
+cumpleCondicionesParaAlegrar(Personaje):-backup(Personaje, Backup), 
+    sano(Backup).
+
+
+sano(Personaje):- not(enfermo(Personaje)).
+
+backup(Personaje, OtroPersonaje):- amigo(Personaje, OtroPersonaje).
+backup(Personaje, OtroPersonaje):- amigo(Personaje, Backup), 
+    backup(Backup, OtroPersonaje).
+
+amigo(campanita, reyesMagos).
+amigo(campanita, conejoDePascua).
+amigo(conejoDePascua, cavenaghi).
+
+enfermo(campanita).
+enfermo(reyesMagos).
+enfermo(conejoDePascua).
 
